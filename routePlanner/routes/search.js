77c70d5b0a5c64a757dd857aed54c5ef67175603
Sullivan <adressecours0.1@gmail.com>
2021-04-28 18:14:35 +0200

    buggy version of the filter option

--- routePlanner/routes/search.js
+++ routePlanner/routes/search.js
@@ -1,15 +1,47 @@
 const router = require("express").Router();
 const Trip = require('../models/Trip');
 
-router.post('/search', (req, res, next) => {
-  res.redirect('/search');
-});
-
 router.get('/search', (req, res, next) => {
-  console.log(req.body)
-  Trip.find()
-  .then(result => res.render('search'))
+  res.render('search');
 })
+
+router.post('/search', (req, res, next) => {
+  const {District, Distance, Mode} = req.body
+  const updatedMode = Mode[0].toUpperCase() + Mode.slice(1)
+  let distanceUpdated ='';
+  switch (Distance) {
+    case '10':
+      Trip.find({$and: [ { District: District }, { Mode: updatedMode },{Distance:{$lte : 9}}]})
+      .then(filteredRoute => {
+        console.log(filteredRoute, '10')
+        res.render('search', {filteredRoute});
+      })
+      .catch(error => req.next(error));
+      case '20':
+      Trip.find({$and: [{ District: District }, { Mode: updatedMode },{$and:[{Distance:{$gte : 10}}, {Distance:{$lte: 20}} ]}]} )
+      .then(filteredRoute => {
+        console.log(filteredRoute, '10-20')
+        res.render('search', {filteredRoute});
+      })
+      .catch(error => req.next(error));
+      case '30':
+        Trip.find({$and: [ { District: District }, { Mode: updatedMode },{Distance:{$lte : 30}}]})
+      .then(filteredRoute => {
+        console.log(filteredRoute, '30')
+        res.render('search', {filteredRoute});
+      })
+      .catch(error => req.next(error));
+      case '31':
+        Trip.find({$and: [ { District: District }, { Mode: updatedMode },{Distance:{$lte : 30}}]})
+        .then(filteredRoute => {
+          console.log(filteredRoute, '31')
+          res.render('search', {filteredRoute});
+        })
+        .catch(error => req.next(error));
+  }
+  console.log(distanceUpdated)
+
+});
 
 module.exports = router;
 
