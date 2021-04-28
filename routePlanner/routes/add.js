79d54366850af380101c6eb23ccec054a01f388c
Chocogrenouille <thestral@gmx.net>
2021-04-28 17:48:13 +0200

    adding form works - needs pictures

--- routePlanner/routes/add.js
+++ routePlanner/routes/add.js
@@ -6,9 +6,19 @@ router.get('/add', (req, res, next) => {
   res.render('add');
 })
 
-router.post('/post', (req, res, next) => {
-  Trip.create()
-  .then()
+router.post('/add', (req, res, next) => {
+  console.log(req.body)
+  const {title, Address, District, Distance, Mode, location, Description} = req.body
+  Trip.create({
+    title: title,
+    Address: Address,
+    District: District,
+    Distance: Distance, 
+    Mode: Mode,
+    location: location,
+    Description: Description
+  })
+  .then(newRoute => res.redirect(`/details/${newRoute._id}`))
   .catch(err => next(err))
 });
 
