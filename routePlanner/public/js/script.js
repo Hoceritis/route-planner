65f2719d3445aba2a21400e25866789d35275e67
Chocogrenouille <thestral@gmx.net>
2021-04-28 12:59:02 +0200

    axios request added

--- routePlanner/public/js/script.js
+++ routePlanner/public/js/script.js
@@ -8,6 +8,7 @@ document.addEventListener(
 
 mapboxgl.accessToken = 'pk.eyJ1IjoiY2hvY29ncmVub3VpbGxlIiwiYSI6ImNrbnlxMDhpbjEycHMyd3RnMWx5dW84NmgifQ.jgYkaT23M3Fo965r73i23A';
 
+//creates new mapbox
 const map = new mapboxgl.Map({
   container: 'map', // container ID
   style: 'mapbox://styles/mapbox/streets-v11', // style URL
@@ -16,18 +17,32 @@ const map = new mapboxgl.Map({
   doubleClickZoom: true
 });
 
-const coords = [13.602637484284998, 52.45813934334384]
-
-const addCoordinates = (coords) => {
-  return coords
-}
-
-
+//creates new navigation set
 const nav = new mapboxgl.NavigationControl();
 map.addControl(nav, 'top-left');
 
-var marker = new mapboxgl.Marker({
-color: "#ffffff",
-draggable: true
-}).setLngLat(addCoordinates(coords))
-.addTo(map);
+const coords = [13.602637484284998, 52.45813934334384]
+
+// This is the array of coordinates for my markers
+let markers = [];
+console.log('first markers: ', markers);
+
+//Gets me the all the coordinates of each route
+axios.get('http://localhost:3000/get-data')
+   .then(results => {
+      const coordinates = [...results.data];
+      coordinates.forEach((coordinate) => {
+        markers.push(coordinate.location.coordinates);
+      })  
+    })
+
+console.log('2nd markers: ', markers);
+// That one should set markers for each route in database
+
+markers.forEach((coords) => {
+  new mapboxgl.Marker({
+    color: "#000000",
+    draggable: true
+    }).setLngLat(coords)
+    .addTo(map);
+})
